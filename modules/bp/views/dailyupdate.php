47ace9c4740baa13f7ab99377de2801ec92758ee
James <james.fearnley@marketforce.com>
2023-02-24 13:14:09 +0000

    Move back to Bootstap 5 to fix the nav menu, update of daily check form to bootstrap 5 classes

--- modules/bp/views/dailyupdate.php
+++ modules/bp/views/dailyupdate.php
@@ -19,42 +19,43 @@
 
     <div class="col-md-10">
         <form method="post">
-            <div class="form-row">
-                <div class="form-group col-md-6">
+            <div class="row">
+                <div class="mb-3 col-md-6">
                     <label for="step">Step Count</label>
                     <input type="number" class="form-control" name="step" id="step" placeholder="90">
                 </div>
-                <div class="form-group col-md-3">
+                <div class="mb-3 col-md-3">
                     <label for="senddaydate">Date of Check</label>
                     <br/>
                     <input type="date" id="senddaydate" name="senddaydate">
                 </div>
-                <div class="form-group col-md-3">
+                <div class="mb-3 col-md-3">
                     <label for="senddaydate">Time of Check</label>
                     <br/>
                     <input type="time" id="senddaytime" name="senddaytime">
                 </div>
             </div>
-            <div class="form-row">
-                <div class="form-group col-md-4">
+            <div class="row">
+                <div class="mb-3 col-md-4">
                     <label for="sys">Sys.mmHG</label>
                     <input type="number" class="form-control" id="sys" name="sys" placeholder="120">
                 </div>
-                <div class="form-group col-md-4">
+                <div class="mb-3 col-md-4">
                     <label for="dia">Dia.mmHg</label>
                     <input type="number" class="form-control" id="dia" name="dia" placeholder="80">
                 </div>
-                <div class="form-group col-md-4">
+                <div class="mb-3 col-md-4">
                     <label for="pul">Pulse</label>
                     <input type="number" class="form-control" id="pul" name="pul" placeholder="60">
                 </div>
             </div>
-            <div class="form-group">
-                <label for="other">Any other information</label>
-                <textarea class="form-control" id="other" name="other" rows="3"></textarea>
+            <div class="row">
+                <div class="mb-3 col-md-12">
+                    <label for="other">Any other information</label>
+                    <textarea class="form-control" id="other" name="other" rows="3"></textarea>
+                </div>
             </div>
-
-            <div class="form-group">
+            <div class="row mb-3">
                 <div class="form-check">
                     <input class="form-check-input" type="checkbox" id="gridCheck">
                     <label class="form-check-label" for="gridCheck">
--- modules/bp/views/header.php
+++ modules/bp/views/header.php
@@ -48,19 +48,17 @@ if (isset($cookies['user'])) {
     <title>Health Diary</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet"
           integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="assets/css/libs/bootstrap-datetimepicker.css">
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css"
-          integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 
-    <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js"
-            integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN"
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
+            integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3"
             crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js"
-            integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q"
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.min.js"
+            integrity="sha384-7VPbUDkoPSGFnVtYi0QogXtr74QeVeeIs99Qfg5YCF+TidwNdjvaKZX19NZ/e6oz"
             crossorigin="anonymous"></script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js"
-            integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl"
-            crossorigin="anonymous"></script>
+
 
     <!--    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.3/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"
