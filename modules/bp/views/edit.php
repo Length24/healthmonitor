7d994d5f5c36d6159881e3047db3740676a2eb59
Lengths <fearnley@gmail.com>
2023-02-27 20:51:16 +0000

    Fix for a couple of bugs

--- modules/bp/views/edit.php
+++ modules/bp/views/edit.php
@@ -75,7 +75,7 @@ if (isset($cookies['ownerId'])) {
             "title": "Delete",
             "data": "download_link",
             "render": function (data, type, row, meta) {
-                return '<a href="' + row[6] + '"><i class="fa fa-trash fa-3" aria-hidden="true"></i></a>';
+                return '<a href="' + row[8] + '"><i class="fa fa-trash fa-3" aria-hidden="true"></i></a>';
             },
         },
         <?php } ?>
@@ -84,7 +84,7 @@ if (isset($cookies['ownerId'])) {
             "className": "dt-body-center dt-head-center", "title": "Edit",
             "data": "download_link",
             "render": function (data, type, row, meta) {
-                return '<a href="' + row[7] + '"><i class="fa fa-pencil-square fa-3" aria-hidden="true"></i></a>';
+                return '<a href="' + row[8] + '"><i class="fa fa-pencil-square fa-3" aria-hidden="true"></i></a>';
             },
         }
         <?php } ?>
